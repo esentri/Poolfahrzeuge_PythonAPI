6256d52b23d73e737f0a7b21fea9b84264380814
Finn Koller <finn.koller@esentri.com>
2020-01-28 15:06:47 +0100

    added CORS

--- python_rest.py
+++ python_rest.py
@@ -7,8 +7,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 #enable CORS 
-#CORS(app, resources={r"/car": {"origins": "*"}})
-CORS(app)
+cors=CORS(app, resources={r"/car": {"origins": "*"}})
+
 # connect to already existing and running Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:@0.0.0.0:5432/esentricar'
 
