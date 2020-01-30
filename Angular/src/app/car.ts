ef30bd246afdb95cab0df4bb3b3e7c925651c53b
Finn Koller <finn.koller@esentri.com>
2020-01-30 11:39:39 +0100

    cleanup

--- Angular/src/app/car.ts
+++
@@ -1,8 +0,0 @@
-export interface Car {
-  car_id: number;
-  license_plate: string;
-  car_type: string;
-  fuel: string;
-  number_of_seats: number;
-
-}
--- Angular/src/app/cars.ts
+++
@@ -1,5 +0,0 @@
-export interface AllCars{
-    car_id: number;
-    license_plate: string;
-    car_type: string;
-}
